658e52fbbdb92e7549ff535976762f681325877d
Dhinakar14 <131858783+Dhinakar14@users.noreply.github.com>
2025-09-20 19:27:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://leocrackers-pgr.netlify.app/"
+    "https://leocrackers-pgr.netlify.app/",
+    "https://leo-crackers.onrender.com"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
